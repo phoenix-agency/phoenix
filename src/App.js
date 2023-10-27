4c9e9f64d72b26df0c7e456e5aa6238f5018c641
Geo <gr.dev.web@gmail.com>
2023-10-27 14:27:34 +0200

    initialisation

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          eLearn React
         </a>
       </header>
     </div>
