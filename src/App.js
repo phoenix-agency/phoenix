80e7ec5dde6a3c5941448ad99940f4d507600479
ACH90 <chassagne.alexandre@gmail.com>
2023-10-30 16:52:26 +0100

    "Rectification Architecture"

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import './App.css';
+//Import Router
 import { Routes, Route } from 'react-router-dom';
-import Accueil from './pages/Accueil';
-import Presentation from './pages/Presentation';
-import Prestations from './pages/Prestations';
-import Relalisations from './pages/Relalisations';
-import Contacts from './pages/Contacts';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import NavBar from './components/NavBar';
+//Import Pages
+import Accueil from './pages/Accueil/Accueil';
+import Presentation from './pages/Presentation/Presentation';
+import Prestations from './pages/Prestations/Prestations';
+import Realisations from './pages/Realisations/Realisations';
+import Contacts from './pages/Contact/Contacts';
+//Import Components
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import NavBar from './components/NavBar/NavBar';
+
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <NavBar />
       <Routes>
         <Route path='/' element={<Accueil />} />
         <Route path='/presentation' element={<Presentation />} />
         <Route path='/prestations' element={<Prestations />} />
-        <Route path='/relalisations' element={<Relalisations />} />
+        <Route path='/realisations' element={<Realisations />} />
         <Route path='/contacts' element={<Contacts />} />
       </Routes>
       <Footer />
-    </div>
+    </>
   );
 }
 
--- src/pages/Relalisations.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Relalisations() {
-  return (
-    <div>Relalisations</div>
-  )
-}
