47f025e061f4b370e96ffa4e87dff3e446bd748b
ACH90 <chassagne.alexandre@gmail.com>
2023-10-31 10:20:34 +0100

    "Header"

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <div>
+        <div>Email : Dodolasaumure@gmail.com</div>
+        <div>Tel : 06 12 34 56 78</div>
+      </div>
+      <div>
+        <a href="https://github.com" target="_blank" rel="noreferrer">github</a>
+      </div>
+    </div>
   )
 }
