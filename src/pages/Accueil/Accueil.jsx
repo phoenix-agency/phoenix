d655fe16616aa578238f00ec0354175f917b08d3
ACH90 <chassagne.alexandre@gmail.com>
2023-11-08 10:39:22 +0100

    Création Component "Responsive"

--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -2,6 +2,7 @@ import NosClients from "../../components/Home/NosClients";
 import Society from "./Components/Society.jsx";
 import CloseToC from "./Components/CloseToC.jsx";
 import Partner from "./Components/Partner.jsx";
+import Responsive from "./Components/Responsive.jsx";
 
 export default function Accueil() {
   return (
@@ -9,6 +10,7 @@ export default function Accueil() {
       <Society />
       <CloseToC />
       <Partner />
+      <Responsive />
       <NosClients />
     </div>
   );
---
+++ src/pages/Accueil/Components/Responsive.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import responsivePic from './ComponentsPic/Responsive.png'
+
+export default function responsive() {
+  return (
+    <div>
+        <div>
+            <h1>Conception De Site Internet Responsive Design À Lille 3 En 1</h1>
+            <h3> site internet opérationnel à la fois sur mobile, ordinateur & tablette</h3>
+        </div>
+        <div>
+            <div>
+                <h2>Développement Premium</h2>
+                <p>
+                    Nous prêtons une attention particulière à la conception de pointe et les normes 
+                    d'utilisabilité modernes.
+                </p>
+            </div>
+            <div>
+                <h2>Cross Plateform & Responsive Design</h2>
+                <p>
+                    Nos sites internet responsive design s'adaptent librement et efficacement 
+                    sur tous les périphériques de n'importe qu'elle marque.
+                </p>
+            </div>
+            <div>
+                <h2>
+                    Optimisation Accrue
+                </h2>
+                <p>
+                    Nous avons mis en place une stratégie qui permet à nos conceptions web d'accélérer 
+                    efficacement l'affichage de votre site internet.
+                </p>
+            </div>
+        </div>
+        <div>
+            <img src= {responsivePic} alt="Responsive" />
+        </div>
+        <div>
+            <div>
+                <h2>
+                    Compatible Iphone , Android & Windows Phone
+                </h2>
+                <p>
+                    Nos sites internet responsive design fonctionnent sur toutes les plateformes et 
+                    s'adaptent automatiquement.
+                </p>
+            </div>
+            <div>
+                <h2>
+                    Expérience Utilisateur Ergonomique
+                </h2>
+                <p>
+                    Une ergonomie optimale pour un meilleur confort - Rapidité d'affichage - Navigation sécurisée.
+                </p>
+            </div>
+            <div>
+                <h2>
+                    Préconisé Par Google
+                </h2>
+                <p>
+                    Google a imposé la standardisation du responsive design pour le confort des utilisateurs. 
+                    En respectant cette règle vous aurez, automatiquement, un meilleur référencement naturel (SEO).
+                </p>
+            </div>
+        </div>
+    </div>
+  )
+}
