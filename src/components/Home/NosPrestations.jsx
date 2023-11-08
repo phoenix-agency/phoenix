413361e868bdf53729a2f1aaec94a3505d577ef6
HafewaBargaoui <hafewa.bargaoui@gmail.com>
2023-11-08 16:48:07 +0100

    composant NosPrestations page d'accueil

---
+++ src/components/Home/NosPrestations.jsx
@@ -0,0 +1,89 @@
+import conception from "../../assets/conception.png";
+import developpement from "../../assets/developpement.png";
+import referencement from "../../assets/referencement.png";
+import marketing from "../../assets/marketing.png";
+import Button from "../Button/Button";
+const NosPrestations = () => {
+  return (
+    <div>
+      <h2 className="mt-16 capitalize text-center font-bold text-secondary text-5xl">
+        Nos prestations web et digitales
+      </h2>
+      <p className="mx-72 text-center text-gray-dark my-4">
+        Nous offrons un éventail varié de services en web marketing, en
+        développement web et marketing numérique (E-commerce). Nos prestations
+        englobent le référencement (SEO-SEA), la conception de site internet, le
+        marketing des réseaux sociaux (Social Média Marketing) et bien plus
+        encore.
+      </p>
+      <div className="mx-6 pt-5 grid grid-flow-row gap-8 sm:grid-cols-2">
+        <div className="p-14 border border-gray-200 grid grid-flow-row justify-items-center hover:border-primary duration-300">
+          <img
+            className="h-52 hover:scale-105 duration-200"
+            src={conception}
+            alt="Conception"
+          />
+          <h3 className="text-2xl uppercase text-secondary font-semibold my-4">
+            Conception de site internet
+          </h3>
+          <p className="text-gray-dark text-center">
+            Posséder un site internet inimitable est un incontournable. Il
+            représente votre entreprise et vos valeurs. Il est un incroyable
+            accélérateur de business.
+          </p>
+          <Button color="bg-primary" title="En savoir plus" link="#" />
+        </div>
+        <div className="p-14 border border-gray-200 grid grid-flow-row justify-items-center hover:border-primary duration-300">
+          <img
+            className="h-52 hover:scale-105 duration-200"
+            src={developpement}
+            alt="développement"
+          />
+          <h3 className="text-2xl uppercase text-secondary font-semibold my-4">
+            Développement web
+          </h3>
+          <p className="text-gray-dark text-center">
+            Nous serons un renfort technique et technologique pour la
+            communication de votre entreprise au travers le déploiement de
+            technologies modernes, robustes et sécurisées.
+          </p>
+          <Button color="bg-primary" title="En savoir plus" link="#" />
+        </div>
+        <div className="p-14 border border-gray-200 grid grid-flow-row justify-items-center hover:border-primary duration-300">
+          <img
+            className="h-52 hover:scale-105 duration-200"
+            src={referencement}
+            alt="Référencement"
+          />
+          <h3 className="text-2xl uppercase text-secondary font-semibold my-4">
+            Optimisation seo
+          </h3>
+          <p className="text-gray-dark text-center">
+            Webagencelille.com est destinée aux clients qui ont la nécessité
+            d&apos;un accompagnement intensif et complet en référencement
+            naturel, Accélération d&apos;affichage du site, optimisation du code
+            source et plus encore..
+          </p>
+          <Button color="bg-primary" title="En savoir plus" link="#" />
+        </div>
+        <div className="p-14 border border-gray-200 grid grid-flow-row justify-items-center hover:border-primary duration-300">
+          <img
+            className="h-52 hover:scale-105 duration-200"
+            src={marketing}
+            alt="Marketing"
+          />
+          <h3 className="text-2xl uppercase text-secondary font-semibold my-4">
+            Social media marketing
+          </h3>
+          <p className="text-gray-dark text-center">
+            Une aide précieuse pour vos démarches avec les publicités et la
+            communication sur les réseaux sociaux.
+          </p>
+          <Button color="bg-primary" title="En savoir plus" link="#" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NosPrestations;
--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -1,12 +1,14 @@
 import NosClients from "../../components/Home/NosClients";
 import Society from "./Components/Society.jsx";
 import CloseToC from "./Components/CloseToC.jsx";
+import NosPrestations from "../../components/Home/NosPrestations.jsx";
 
 export default function Accueil() {
   return (
     <div>
       <Society />
       <CloseToC />
+      <NosPrestations />
       <NosClients />
     </div>
   );
