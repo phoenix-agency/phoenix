af03528d212eff2e0ada71ce80a91bf9dfa64bcb
ACH90 <chassagne.alexandre@gmail.com>
2023-11-08 10:12:38 +0100

    component Partner

--- src/pages/Accueil/Accueil.jsx
+++ src/pages/Accueil/Accueil.jsx
@@ -1,12 +1,14 @@
 import NosClients from "../../components/Home/NosClients";
 import Society from "./Components/Society.jsx";
 import CloseToC from "./Components/CloseToC.jsx";
+import Partner from "./Components/Partner.jsx";
 
 export default function Accueil() {
   return (
     <div>
       <Society />
       <CloseToC />
+      <Partner />
       <NosClients />
     </div>
   );
