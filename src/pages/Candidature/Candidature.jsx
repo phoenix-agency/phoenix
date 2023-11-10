a14829450bb02c331fac5347688b5214fbf05aca
ACH90 <chassagne.alexandre@gmail.com>
2023-11-10 17:01:44 +0100

    FormCandidature CSS

--- src/pages/Candidature/Candidature.jsx
+++ src/pages/Candidature/Candidature.jsx
@@ -2,10 +2,13 @@ import FormCandidature from '../../components/FormCandidature/FormCandidature'
 
 export default function Candidature() {
   return (
-    <div>Recherche Stage / Contrat pro / 
+    <div>
+      <div>Recherche Stage / Contrat pro / 
         Alternance Webdesign développeur web, communication, Référencement, à Lille : 
         Dépôt de la candidature 
+      </div>
         <FormCandidature/>
+
     </div>
   )
 }
