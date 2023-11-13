5582b795b9de3f0705c6522ae2806dbf81c07925
Geo <gr.dev.web@gmail.com>
2023-11-13 17:33:15 +0100

    Bouton Loupe Fonctionnel / Affichage fait

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,11 +5,17 @@ import Logo from './../../assets/LogoBlanc.png';
 import ListePresta from './ListePresta';
 import ListeContact from './ListeContact';
 import Loupe from './../../assets/svg/Loupe';
+import Research from './../../components/Research/Research';
 
 
 export default function NavBar() {
   const [showPrestaList, setShowPrestaList] = useState(false);
   const [showContactList, setShowContactList] = useState(false);
+  const [showResearch, setShowResearch] = useState(false);
+
+  const handleLoupeClick = () => {
+    setShowResearch(true); 
+  };
 
   return (
     <nav>
@@ -26,7 +32,10 @@ export default function NavBar() {
         onMouseLeave={() => setShowContactList(false)}>Contacts
         {showContactList && <ListeContact />}
         </Link>
-        <Loupe />  
+        <div className="Loupe" onClick={handleLoupeClick}>
+          <Loupe /> 
+        </div>
+        {showResearch && <Research />}
       </div>
     </nav>
   );
---
+++ src/components/Research/Research.jsx
@@ -0,0 +1,19 @@
+import CloseButton from "../../assets/svg/CloseButton"
+import Loupe from "../../assets/svg/Loupe"
+import  "./ResearchStyle.css"
+
+function Research () {
+    return (
+        <div className="Research-Container">
+            <ul className="Research">
+                <li><CloseButton /></li>
+                <li className="Research-box">
+                    <input className="Research-Holder" placeholder="Recherche" />
+                    <Loupe/>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Research
