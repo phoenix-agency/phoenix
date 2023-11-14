8f29eaf34cfa73ef64aabe06443ede9b9cb1f89f
Geo <gr.dev.web@gmail.com>
2023-11-14 12:12:25 +0100

    Header Fonc / 1er style terminé

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,20 @@
+import GitHubSvg from '../../assets/svg/GitHubSvg'
 import './HeaderStyle.css'
 export default function Header() {
   return (
     <div className="Header">
-        <div>Email : Dodolasaumure@gmail.com</div>
-        <div>Tel : 06 12 34 56 78</div>
-        <a href="https://github.com" target="_blank" rel="noreferrer">github</a>
+        <div className='HeaderContainer'>
+          <div className='Row Width17 Padding'>
+            <p>Email : </p>
+            <a href="mailto:Dodolasaumure@gmail.com"><p className='Grey'>Dodolasaumure@gmail.com</p></a>
+          </div>
+          <div className='Row Width10 Border Padding'>
+            <p>Tel : </p>
+            <a href="tel:0612345678"><p className='Grey'>06 12 34 56 78</p></a>
+            
+          </div>
+        </div>
+        <a href="https://github.com" target="_blank" rel="noreferrer"><GitHubSvg /></a>
     </div>
   )
 }
