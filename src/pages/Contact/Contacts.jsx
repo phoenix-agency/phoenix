67e98a3fcfef33a7ac975e6f37dd838b8b7d4e87
ACH90 <chassagne.alexandre@gmail.com>
2023-11-17 16:40:59 +0100

    Page Contact Build

--- src/pages/Contact/Contacts.jsx
+++ src/pages/Contact/Contacts.jsx
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default function Contacts() {
   return (
-    <div>Contacts</div>
+    <div>
+    <section className="ContactsHeader">
+      <h2>
+        Restons en contact.<br />
+        Présentez votre projet.
+      </h2>
+      <h3>
+        Nos devis sont gratuits
+      </h3>
+      <p>
+        Découvrir le formulaire
+      </p>
+    </section>
+    <section className="ContactsMain">
+      </section>
+      <section className="ContactsFooter">
+
+      </section>
+    </div>
   )
 }
