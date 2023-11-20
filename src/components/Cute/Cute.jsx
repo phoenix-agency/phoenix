5826198ace573cefd6e0e88f337563adbf25a247
Geo <gr.dev.web@gmail.com>
2023-11-20 23:14:28 +0100

    Pge Wordpress / Sect 2  done/ Partie Cute done

--- src/components/Cute/Cute.jsx
+++ src/components/Cute/Cute.jsx
@@ -1,6 +1,6 @@
 import CutePic from "./../../assets/CutePic.png";
-import ChiffreQuatre from "./../../assets/ChiffreDeux.png";
-import BoutonRond from "./../../assets/svg/BoutonRond";
+import ChiffreQuatre from "./../../assets/ChiffreQuatre.png";
+import { Link } from "react-router-dom";
 
 function cute() {
     return (
@@ -13,7 +13,7 @@ function cute() {
                     <h1 className="Title-Wordpress">WordPress : une technologie accessible mais qui demande des connaissances en développement web.</h1>
                     <p className="Text-Wordpress">Nous sommes bien conscients qu&apos;aujourd&apos;hui avec le web, vous pouvez facilement trouver des tutos pour faire vous-même votre site internet. Cependant WordPress reste un outil qui est peu optimisé et sécurisé, faisant ainsi ralentir la vitesse d&apos;affichage de votre site internet et permettant aux hackers de prendre le contrôle des données de vos clients. C&apos;est la raison pour laquelle, avec notre agence web de Lille, nous avons spécialement mis en place diverses stratégies qui permettent d&apos;accélérer et d&apos;optimiser considérablement le chargement de votre code source.</p>
                     <div className="Container-Wordpress-Button">
-                        <BoutonRond />        
+                        <Link to="/wordpress" className="Bouton-Rond">OK JE VEUX EN SAVOIR PLUS</Link>
                     </div>
                 </div>
             </div>
