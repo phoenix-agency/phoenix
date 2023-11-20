5da84233a32bbd3eef2dc75b7fe20794fa9540d3
Geo <gr.dev.web@gmail.com>
2023-11-20 23:21:50 +0100

    Pge Wordpress / Sect 2 done / Partie Cute done

--- src/components/Cute/Cute.jsx
+++ src/components/Cute/Cute.jsx
@@ -10,8 +10,8 @@ function cute() {
                     <img src={ChiffreQuatre} alt='Illustration chiffre deux' className='Chiffre-Wordpress' />
                 </div>
                 <div className="Presentation-Wordpress">
-                    <h1 className="Title-Wordpress">WordPress : une technologie accessible mais qui demande des connaissances en développement web.</h1>
-                    <p className="Text-Wordpress">Nous sommes bien conscients qu&apos;aujourd&apos;hui avec le web, vous pouvez facilement trouver des tutos pour faire vous-même votre site internet. Cependant WordPress reste un outil qui est peu optimisé et sécurisé, faisant ainsi ralentir la vitesse d&apos;affichage de votre site internet et permettant aux hackers de prendre le contrôle des données de vos clients. C&apos;est la raison pour laquelle, avec notre agence web de Lille, nous avons spécialement mis en place diverses stratégies qui permettent d&apos;accélérer et d&apos;optimiser considérablement le chargement de votre code source.</p>
+                    <h1 className="Title-Wordpress">Oui d&apos;accord, vous êtes bien mignons dans votre agence web de Lille mais nous on n&apos;aime pas WordPress. Quelqu&apos;un m&apos;a dit que Drupal ou Joomla sont mieux que WordPress</h1>
+                    <p className="Text-Wordpress">D&apos;abord il faut savoir que tous les CMS se valent. chacun présente autant d&apos;avantages que d&apos;inconvénients. Aucun CMS n&apos;est meilleur que l&apos;autre. Il faut surtout auditer et bien réfléchir sur les tenants et les aboutissants de la création de votre site internet. Mais rassurez-vous WEBAGENCELILLE.COM ne fait pas que du WordPress, nous faisons aussi du Joomla , du Drupal et aussi du développement à base de framework comme Boostrap.</p>
                     <div className="Container-Wordpress-Button">
                         <Link to="/wordpress" className="Bouton-Rond">OK JE VEUX EN SAVOIR PLUS</Link>
                     </div>
