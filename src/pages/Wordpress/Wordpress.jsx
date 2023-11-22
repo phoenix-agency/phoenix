5e4a0bfa9d9e8099348fbac706d97f004f7b4d67
Geo <gr.dev.web@gmail.com>
2023-11-22 12:04:02 +0100

    Pge Wordpress / Part 1-2-3-4-5 done

--- src/pages/Wordpress/Wordpress.jsx
+++ src/pages/Wordpress/Wordpress.jsx
@@ -2,6 +2,7 @@ import WordpressExpert from "../../components/WordpressExpert/WordpressExpert"
 import WhyWordpress from "../../components/WhyWordpress/WhyWordpress"
 import Processus from "../../components/Processus/Processus"
 import YourWebsite from "../../components/YourWebsite/YourWebsite"
+import WordpressServices from "../../components/WordpressServices/WordpressServices"
 import "./WordpressStyle.css"
 
 function wordpress() {
@@ -11,6 +12,7 @@ function wordpress() {
             <WhyWordpress />
             <Processus />
             <YourWebsite />
+            <WordpressServices />
         </div>
     )
 }
