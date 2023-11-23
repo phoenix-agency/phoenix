96c883f005efb19488cd7a09572199a0a57af7d3
ACH90 <chassagne.alexandre@gmail.com>
2023-11-23 10:39:06 +0100

    Page Realisations

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,11 @@
-import './Navbar.css';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import Logo from './../../assets/LogoBlanc.png';
-import ListePresta from './ListePresta';
-import ListeContact from './ListeContact';
-import Loupe from './../../assets/svg/Loupe';
-import Research from './../../components/Research/Research';
-
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import Logo from "./../../assets/LogoBlanc.png";
+import ListePresta from "./ListePresta";
+import ListeContact from "./ListeContact";
+import Loupe from "./../../assets/svg/Loupe";
+import Research from "./../../components/Research/Research";
 
 export default function NavBar() {
   const [showPrestaList, setShowPrestaList] = useState(false);
@@ -14,26 +13,36 @@ export default function NavBar() {
   const [showResearch, setShowResearch] = useState(false);
 
   const handleLoupeClick = () => {
-    setShowResearch(true); 
+    setShowResearch(true);
   };
 
   return (
     <nav>
       <div className="Navbar">
-        <Link to="/"><img src={Logo} alt="logo" className='Logo' /></Link> 
+        <Link to="/">
+          <img src={Logo} alt="logo" className="Logo" />
+        </Link>
         <Link to="/">Accueil</Link>
         <Link to="/presentation">Qui sommes-nous</Link>
-        <Link to='/prestations' onMouseEnter={() => setShowPrestaList(true)}
-          onMouseLeave={() => setShowPrestaList(false)}>Prestations
+        <Link
+          to="/prestations"
+          onMouseEnter={() => setShowPrestaList(true)}
+          onMouseLeave={() => setShowPrestaList(false)}
+        >
+          Prestations
           {showPrestaList && <ListePresta />}
         </Link>
-        <Link to="/realisations">Réalisations</Link>
-        <Link to="/contacts" onMouseEnter={() => setShowContactList(true)}
-        onMouseLeave={() => setShowContactList(false)}>Contacts
-        {showContactList && <ListeContact />}
+        <Link to="https://projet-wal.fr/">Réalisations</Link>
+        <Link
+          to="/contacts"
+          onMouseEnter={() => setShowContactList(true)}
+          onMouseLeave={() => setShowContactList(false)}
+        >
+          Contacts
+          {showContactList && <ListeContact />}
         </Link>
         <div className="Loupe" onClick={handleLoupeClick}>
-          <Loupe /> 
+          <Loupe />
         </div>
         {showResearch && <Research />}
       </div>
